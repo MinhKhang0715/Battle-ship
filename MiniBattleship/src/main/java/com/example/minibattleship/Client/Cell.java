3be47f5e1e207f376016c62af8403fce8c55ad14
MinhKhang0715 <nkhang982@gmail.com>
2022-12-07 11:56:38 +0700

    Temporally fixed shoot function, there're still some minor issues

--- MiniBattleship/src/main/java/com/example/minibattleship/Client/Cell.java
+++ MiniBattleship/src/main/java/com/example/minibattleship/Client/Cell.java
@@ -7,7 +7,7 @@ public class Cell extends Rectangle {
     //    coordinates
     public int x, y;
     //    does the cell contain a ship, and has it been shot yet?
-    public boolean containShip = false, wasShot = false;
+    private boolean containShip = false, wasShot = false;
 
     public Cell(int x, int y) {
 //        calling super for Rectangle constructor which lets us set the size of each cell
@@ -18,13 +18,27 @@ public class Cell extends Rectangle {
         this.setStroke(Color.BLACK);
     }
 
-    //to differentiate our board and the enemy's board
-    public Cell(int x, int y, boolean isMyself) {
-        super(30, 30);
-        this.x = x;
-        this.y = y;
-        this.setFill(Color.BLUE);
-        this.setStroke(Color.BLACK);
+    public void setContainShip(boolean containShip) {
+        this.containShip = containShip;
+        if (this.containShip)
+            this.setFill(Color.YELLOW);
     }
+
+    public void setWasShot(boolean wasShot) {
+        this.wasShot = wasShot;
+        if (this.wasShot && this.containShip)
+            this.setFill(Color.CRIMSON);
+        if (this.wasShot && !this.containShip)
+            this.setFill(Color.GREY);
+    }
+
+    public boolean isContainShip() {
+        return containShip;
+    }
+
+    public boolean getWasShot() {
+        return wasShot;
+    }
+
 
 }
--- MiniBattleship/src/main/java/com/example/minibattleship/Client/Controllers/GamePanel.java
+++ MiniBattleship/src/main/java/com/example/minibattleship/Client/Controllers/GamePanel.java
@@ -6,14 +6,13 @@ import com.example.minibattleship.Client.TCPConnection;
 import com.example.minibattleship.Helper.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-
-import java.util.Arrays;
 
 public class GamePanel {
     private final TCPConnection tcpConnection;
@@ -44,7 +43,6 @@ public class GamePanel {
     public void initialize() {
         state = GameState.PlacingShip;
         placeMyShip();
-        populateEnemyBoard();
         enemyBoard.setDisable(true);
     }
 
@@ -61,53 +59,78 @@ public class GamePanel {
         enemyCoordinate = coordinateFromServer.getMessage().split(",");
         enemyBoard.setDisable(false);
         myBoard.setDisable(true);
+        populateEnemyBoardTest();
+        battling();
         btnDonePlacingShip.setDisable(true);
     }
 
-    private void populateEnemyBoard() {
+    private void populateEnemyBoardTest() {
         for (int y = 0; y < size; y++) {
             HBox row = new HBox();
             for (int x = 0; x < size; x++) {
                 Cell cell = new Cell(x, y);
-                cell.setOnMouseClicked(mouseEvent -> {
-                    cell.wasShot = true;
-                    String shotCoordinate = cell.x + Integer.toString(cell.y);
-                    if (Arrays.asList(enemyCoordinate).contains(shotCoordinate)) {
-                        System.out.println("HIT!!!");
-                        cell.setFill(Color.CRIMSON);
-                        tcpConnection.sendMessage(new User().setUsername(username).setGameState("Battling").setMessage("Hit," + shotCoordinate));
-                        enemyShips--;
-                        if (enemyShips == 0) {
-                            alert("Winner", "Congratulation, you won!!", "You have destroyed all enemy's ships");
-                            enemyBoard.setDisable(true);
-                            myBoard.setDisable(true);
-                        }
-                    } else {
-                        System.out.println("MISSED");
-                        cell.setFill(Color.GREY);
-                        tcpConnection.sendMessage(new User().setUsername(username).setGameState("Battling").setMessage("Missed," + shotCoordinate));
+                for (String coordinate : enemyCoordinate) {
+                    if (coordinate.equals(x + Integer.toString(y))) {
+                        cell.setContainShip(true);
+                        cell.setFill(Color.WHITESMOKE);
+                        break;
                     }
-                    enemyBoard.setDisable(true);
-                    System.out.println("Waiting for enemy's shot");
-                    User fromServer = (User) tcpConnection.readMessage();
-                    String[] enemyShotCoordinate = fromServer.getMessage().split(",");
-                    int xCoordinate = Integer.parseInt(String.valueOf(enemyShotCoordinate[1].charAt(0)));
-                    int yCoordinate = Integer.parseInt(String.valueOf(enemyShotCoordinate[1].charAt(1)));
-                    if (enemyShotCoordinate[0].equals("Hit")) {
-                        ((Cell) ((HBox) myBoard.getChildren().get(yCoordinate)).getChildren().get(xCoordinate)).setFill(Color.CRIMSON);
-                        remains--;
-                        System.out.println("Ship remains: " + remains);
-                        if (remains == 0) {
-                            alert("Too bad", "You lost", "All of your ships have been destroyed by the enemy");
-                        }
-                    } else if (enemyShotCoordinate[0].equals("Missed")) {
-                        ((Cell) ((HBox) myBoard.getChildren().get(yCoordinate)).getChildren().get(xCoordinate)).setFill(Color.GREY);
-                        enemyBoard.setDisable(false);
-                    }
-                });
+                }
                 row.getChildren().add(cell);
             }
             enemyBoard.getChildren().add(row);
+        }
+    }
+
+    private void battling() {
+        for (Node node : enemyBoard.getChildren()) {
+            HBox row = (HBox) node;
+            for (Node nodeCell : row.getChildren()) {
+                Cell cell = (Cell) nodeCell;
+                cell.setOnMouseClicked(mouseEvent -> {
+                    System.out.println("SHOT!!");
+                    String shotCoordinate = cell.x + Integer.toString(cell.y);
+                    String shotResult = placeShot(cell.x, cell.y);
+                    System.out.println(shotResult);
+                    if (shotResult.equals("Already shot")) {
+                        System.out.println("Already shot");
+                        alert("Attention!", "Invalid Shot!!", "The cell had already been shot");
+                    }
+                    else {
+                        if (shotResult.equals("Hit")) {
+                            System.out.println("HIT");
+                            enemyShips--;
+                            if (enemyShips == 0) {
+                                alert("Winner", "Congratulation, you won!!", "You have destroyed all enemy's ships");
+                                enemyBoard.setDisable(true);
+                                myBoard.setDisable(true);
+                            } else tcpConnection.sendMessage(new User().setUsername(username).setGameState("Battling").setMessage("Hit," + shotCoordinate));
+                        } else if (shotResult.equals("Missed")) {
+                            System.out.println("MISSED");
+                            tcpConnection.sendMessage(new User().setUsername(username).setGameState("Battling").setMessage("Missed," + shotCoordinate));
+                        }
+                        System.out.println("End of placeShot");
+                        enemyBoard.setDisable(true);
+                        System.out.println("Waiting for enemy's shot");
+                        User fromServer = (User) tcpConnection.readMessage();
+                        System.out.println("Username: " + fromServer.getUsername() + " Message: " + fromServer.getMessage());
+                        String[] enemyShotCoordinate = fromServer.getMessage().split(",");
+                        int xCoordinate = Integer.parseInt(String.valueOf(enemyShotCoordinate[1].charAt(0)));
+                        int yCoordinate = Integer.parseInt(String.valueOf(enemyShotCoordinate[1].charAt(1)));
+                        ((Cell) ((HBox) myBoard.getChildren().get(yCoordinate)).getChildren().get(xCoordinate)).setWasShot(true);
+                        if (enemyShotCoordinate[0].equals("Hit")) {
+                            remains--;
+                            System.out.println("Ship remains: " + remains);
+                            if (remains == 0) {
+                                alert("Too bad", "You lost", "All of your ships have been destroyed by the enemy");
+                            }
+                            enemyBoard.setDisable(false);
+                        } else if (enemyShotCoordinate[0].equals("Missed")) {
+                            enemyBoard.setDisable(false);
+                        }
+                    }
+                });
+            }
         }
     }
 
@@ -141,12 +164,21 @@ public class GamePanel {
     private boolean place(int x, int y) {
         if (isValidToPlace(x, y)) {
             Cell cell = getCellFromMyBoard(x, y);
-            if (!cell.containShip) {
+            if (!cell.isContainShip()) {
                 cell.setFill(Color.YELLOW);
-                cell.containShip = true;
+                cell.setContainShip(true);
                 return true;
             } else return false;
         } else return false;
+    }
+
+    private String placeShot(int x, int y) {
+        Cell cell = getCellFromEnemyBoard(x, y);
+        if (!cell.getWasShot()) {
+            cell.setWasShot(true);
+            if (cell.isContainShip()) return "Hit";
+            else return "Missed";
+        } else return "Already shot";
     }
 
     private boolean isValidToPlace(int x, int y) {
@@ -155,6 +187,10 @@ public class GamePanel {
 
     private Cell getCellFromMyBoard(int x, int y) {
         return (Cell) ((HBox) myBoard.getChildren().get(y)).getChildren().get(x);
+    }
+
+    private Cell getCellFromEnemyBoard(int x, int y) {
+        return (Cell) ((HBox) enemyBoard.getChildren().get(y)).getChildren().get(x);
     }
 
     private void alert(String title, String headerText, String contentText) {
